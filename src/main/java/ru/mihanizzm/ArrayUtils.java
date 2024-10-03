cbe77fff9dcdec438bfa47068b158b8f8303f88d
Mikhail Sartakov <m.sartakov@g.nsu.ru>
2024-10-03 15:00:34 +0700

    deleted unused code

--- src/main/java/ru/mihanizzm/ArrayUtils.java
+++ src/main/java/ru/mihanizzm/ArrayUtils.java
@@ -10,19 +10,7 @@ public class ArrayUtils {
         arr[j] = temp;
     }
 
-    public static <T> void reverse(T[] arr) {
-        for (int i = 0; i < arr.length / 2; i++) {
-            swap(arr, i, arr.length - i - 1);
-        }
-    }
-
     public static <T> void shuffle(T[] arr) {
         Collections.shuffle(Arrays.asList(arr));
-    }
-
-    public static <T> T[] copy(T[] arr) {
-        T[] copy = (T[]) new Object[arr.length];
-        System.arraycopy(arr, 0, copy, 0, arr.length);
-        return copy;
     }
 }
